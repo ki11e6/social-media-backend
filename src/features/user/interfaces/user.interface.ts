3c2106b9e14cb9b8d9ec314f090f2cbfaa76636f
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-09 22:12:32 +0530

    user interface and model

---
+++ src/features/user/interfaces/user.interface.ts
@@ -0,0 +1,120 @@
+import mongoose, { Document } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+//optional properties are only for Redis and will not be stored in mongoose, instead reference
+export interface IUserDocument extends Document {
+  _id: string | ObjectId;
+  authId: string | ObjectId;
+  username?: string;
+  email?: string;
+  password?: string;
+  avatarColor?: string;
+  uId?: string;
+  postsCount: number;
+  work: string;
+  school: string;
+  quote: string;
+  location: string;
+  //blocked by user
+  blocked: mongoose.Types.ObjectId[];
+  //blocked by other user
+  blockedBy: mongoose.Types.ObjectId[];
+  followersCount: number;
+  followingCount: number;
+  notifications: INotificationSettings;
+  social: ISocialLinks;
+  bgImageVersion: string;
+  bgImageId: string;
+  profilePicture: string;
+  passwordResetToken?: string;
+  passwordResetExpires?: number | string;
+  createdAt?: Date;
+}
+
+export interface IUser {
+  _id: string | ObjectId;
+  authId: string | ObjectId;
+  uId: string;
+  username: string;
+  email: string;
+  password?: string;
+  avatarColor: string;
+  createdAt: Date;
+  postsCount: number;
+  work: string;
+  school: string;
+  quote: string;
+  location: string;
+  blocked: mongoose.Types.ObjectId[];
+  blockedBy: mongoose.Types.ObjectId[];
+  followersCount: number;
+  followingCount: number;
+  notifications: INotificationSettings;
+  social: ISocialLinks;
+  bgImageVersion: string;
+  bgImageId: string;
+  profilePicture: string;
+  passwordResetToken?: string;
+  passwordResetExpires?: number | string;
+}
+
+export interface IResetPasswordParams {
+  username: string;
+  email: string;
+  ipaddress: string;
+  date: string;
+}
+
+//only when true user get notifications
+export interface INotificationSettings {
+  messages: boolean;
+  reactions: boolean;
+  comments: boolean;
+  follows: boolean;
+}
+
+export interface IBasicInfo {
+  quote: string;
+  work: string;
+  school: string;
+  location: string;
+}
+
+export interface ISocialLinks {
+  facebook: string;
+  instagram: string;
+  twitter: string;
+  youtube: string;
+}
+
+export interface ISocketData {
+  blockedUser: string;
+  blockedBy: string;
+}
+
+export interface ILogin {
+  userId: string;
+}
+
+export interface IUserJobInfo {
+  key?: string;
+  value?: string | ISocialLinks;
+}
+
+export interface IUserJob {
+  keyOne?: string;
+  keyTwo?: string;
+  key?: string;
+  value?: string | INotificationSettings | IUserDocument;
+}
+
+export interface IEmailJob {
+  receiverEmail: string;
+  template: string;
+  subject: string;
+}
+
+export interface IAllUsers {
+  users: IUserDocument[];
+  totalUsers: number;
+}
