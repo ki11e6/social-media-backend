9d00a82c7ecb4c164b5486ff522fc069dacc3e24
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-09 11:44:46 +0530

    joi validation decorator

---
+++ src/shared/globals/decorators/joi-validation.decorators.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { JoiRequestValidationError } from '@global/helpers/error-handler';
+import { Request } from 'express';
+import { ObjectSchema } from 'joi';
+import Logger from 'bunyan';
+import { config } from '@root/config';
+
+const log: Logger = config.createLogger('joi-validator decorator');
+
+//decorator type
+type IJoiDecorator = (target: any, key: string, descriptor: PropertyDescriptor) => void;
+
+//decorator factory function
+export function joiValidation(schema: ObjectSchema): IJoiDecorator {
+  //property descriptor is an object that provides information about a property of an object
+  return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+    //passing (req,res,next)
+    descriptor.value = async function (...args: any[]) {
+      log.warn(`descriptor argument passed ${args}`);
+      const req: Request = args[0];
+      //validate using joi and store error if any in error
+      const { error } = await Promise.resolve(schema.validate(req.body));
+      if (error?.details) {
+        throw new JoiRequestValidationError(error.details[0].message);
+      }
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+}
