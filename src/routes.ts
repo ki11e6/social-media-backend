833f02af5e57be2e8de386e9379c8a8eae631a9b
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-09 16:13:13 +0530

    auth routes-signup

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,11 @@
-import { Application, Request, Response } from 'express';
+import { authRoutes } from '@auth/routes/authRoutes';
+import { Application } from 'express';
 
 const BASE_PATH = '/api/v1';
 
 export default (app: Application) => {
   const routes = () => {
-    app.get(`${BASE_PATH}/hello`, (req: Request, res: Response) => {
-      res.send('Hello World!');
-    });
-    app.post(`${BASE_PATH}/signup`, (req: Request, res: Response) => {
-      // Extract data from the request body
-      const user = req.body;
-      console.log(user);
-
-      // Perform signup logic here (e.g., create a new user in a database)
-      // For this example, we'll just send a response with the received data
-      res.json({
-        message: 'User signed up successfully',
-        user
-      });
-    });
+    app.use(BASE_PATH, authRoutes.routes());
   };
   routes();
 };
