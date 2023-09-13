a1cb1462378465c31222bea679794ec47f2e41a3
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-14 01:00:02 +0530

    socket setup for post

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -14,6 +14,7 @@ import 'express-async-errors';
 import { config } from '@root/config';
 import applicationRoutes from '@root/routes';
 import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
+import { SocketIOPostHandler } from '@socket/post';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -112,8 +113,8 @@ export class AppServer {
     });
   }
   //all sockets io connection will be defined here
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private socketIOConnections(io: Server): void {
-    log.info('socketIOConnections');
+    const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
+    postSocketHandler.listen();
   }
 }
