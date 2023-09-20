7da7c8d65a2dacc9fee9fdff23f93f5926ed0f3f
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-20 14:57:04 +0530

    post get from db

--- src/shared/services/db/post.service.ts
+++ src/shared/services/db/post.service.ts
@@ -1,4 +1,4 @@
-import { IPostDocument } from '@post/interfaces/post.interface';
+import { IGetPostsQuery, IPostDocument } from '@post/interfaces/post.interface';
 import { PostModel } from '@post/models/post.schema';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
@@ -9,6 +9,24 @@ class PostService {
     const post: Promise<IPostDocument> = PostModel.create(createdPost);
     const user: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: 1 } });
     await Promise.all([post, user]);
+  }
+  //Record is an utility object type.here string is key and 1 or -1 is property
+  public async getPosts(query: IGetPostsQuery, skip = 0, limit = 0, sort: Record<string, 1 | -1>): Promise<IPostDocument[]> {
+    let postQuery = {};
+    if (query?.imgId && query?.gifUrl) {
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] };
+    } else if (query?.videoId) {
+      postQuery = { $or: [{ videoId: { $ne: '' } }] };
+    } else {
+      postQuery = query;
+    }
+    const posts: IPostDocument[] = await PostModel.aggregate([{ $match: postQuery }, { $sort: sort }, { $skip: skip }, { $limit: limit }]);
+    return posts;
+  }
+
+  public async postsCount(): Promise<number> {
+    const count: number = await PostModel.find({}).countDocuments();
+    return count;
   }
 }
 
