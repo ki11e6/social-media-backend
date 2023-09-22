6601abda35c0b52e1565531037922861d2061fc9
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-23 00:41:25 +0530

    post auth fixed

--- src/shared/globals/helpers/postAuth-middleware.ts
+++ src/shared/globals/helpers/postAuth-middleware.ts
@@ -6,24 +6,25 @@ export class PostAuthMiddleware {
   public async checkAuthentication(req: Request, _res: Response, next: NextFunction): Promise<void> {
     const userId = req.currentUser?.userId;
     const postId = req.params.postId;
-
     // Check if the user is authenticated
     if (!userId) {
       throw new NotAuthorizedError('Authentication is required to access this route.');
     }
-
     // Check if the post exists
-    const post = await PostModel.findById(postId);
+    let post;
+    try {
+      post = await PostModel.findById(postId);
+    } catch (error) {
+      throw new NotFoundError('Post not found');
+    }
     const postUser = JSON.stringify(post?.userId);
     const curUser = JSON.stringify(userId);
-
-    if (!post) {
-      throw new NotFoundError('Post not found');
-    } else if (postUser !== curUser) {
+    console.log(curUser);
+    console.log(postUser);
+    if (postUser !== curUser) {
       // Check if the authenticated user is the owner of the post
       throw new NotAuthorizedError('You are not authorized to modify this post.');
     }
-
     next();
   }
 }
