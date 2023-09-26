12ee5808c98d47dd000189a5f00dc296662f84fb
ki11e6 <sharath.surendran.m@gmail.com>
2023-09-26 16:27:49 +0530

    comment controller add and get

--- src/features/comments/routes/commentRoutes.ts
+++ src/features/comments/routes/commentRoutes.ts
@@ -1,4 +1,7 @@
 import express, { Router } from 'express';
+import { authMiddleware } from '@global/helpers/auth-middleware';
+import { Get } from '@comment/controllers/get-comments';
+import { Add } from '@comment/controllers/add-comment';
 
 class CommentRoutes {
   private router: Router;
@@ -8,6 +11,12 @@ class CommentRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/post/comments/:postId', authMiddleware.checkAuthentication, Get.prototype.comments);
+    this.router.get('/post/commentsnames/:postId', authMiddleware.checkAuthentication, Get.prototype.commentsNamesFromCache);
+    this.router.get('/post/single/comment/:postId/:commentId', authMiddleware.checkAuthentication, Get.prototype.singleComment);
+
+    this.router.post('/post/comment', authMiddleware.checkAuthentication, Add.prototype.comment);
+
     return this.router;
   }
 }
